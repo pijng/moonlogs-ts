4f0aa1fe60635a9f16801b82cc499c4a0b14ac6c
Mark <shalpack@gmail.com>
2024-02-27 19:13:16 +0300

    Version regression

--- moonlogs-ts/core/OpenAPI.ts
+++ moonlogs-ts/core/OpenAPI.ts
@@ -21,7 +21,7 @@ export type OpenAPIConfig = {
 
 export const OpenAPI: OpenAPIConfig = {
     BASE: '',
-    VERSION: '1.2.5',
+    VERSION: '1.2.4',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
     TOKEN: undefined,
